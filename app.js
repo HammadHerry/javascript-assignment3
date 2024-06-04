7276a5acd9947db2e4d99d4846b6fccba41925c9
HammadHerry <hammadherry81@gmail.com>
2024-06-04 06:35:26 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var caseqty = 
+caseqty = '144'
+console.log(caseqty)
+
+var e = 13
+var f = 21
+w = e-1
+g=f+2
+console.log(w)
+console.log(g)
+
+
+var ordertotal 
+var merchtotal = 100-2;
+var shippingcharge = 21;
+k = merchtotal + shippingcharge
+console.log(k)
+
+
+
+
+
